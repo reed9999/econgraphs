ee3d0f8bd7f5a7c1a44f8b09e285af8545417eef
Philip J. Reed <reed9999@uw.edu>
2017-07-28 00:44:32 -0700

    Set up admin etc.

--- econgraphs/settings.py
+++ econgraphs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'egapp.apps.EgAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- egapp/apps.py
+++ egapp/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class GraphsConfig(AppConfig):
+class EgAppConfig(AppConfig):
     name = 'egapp'
